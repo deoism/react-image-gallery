2b38168781546dcaa1ddff83ba1406a48242a980
deoism <deoism@gmail.com>
2018-02-20 11:03:21 -0500

    added react react-dom webpack wepack-dev-server

---
+++ components/GalleryImage.js
@@ -0,0 +1,10 @@
+// Component for gallery image
+class GalleryImage extends React.Component {
+    render() {
+     return(
+      <img className={this.props.className} src={this.props.src} alt={this.props.alt} />
+     )
+    }
+   }
+
+   export default GalleryImage;
--- image.js
+++ image.js
@@ -1,15 +1,2 @@
 
-var express = require('express');
-
-var app = express();
  
-var Gallery = require('express-photo-gallery');
-  
-var options = {
-  title: 'My Awesome Photo Gallery'
-};
- 
-app.use('/', Gallery('./images', options));
-console.log(app.options);
- console.log("searching");
-app.listen(3000);
